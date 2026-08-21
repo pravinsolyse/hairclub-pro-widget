1ac1b0e5de054a01dd41b2f0bd2cee5e7fe7bf7e
pravinsolyse <318328232+pravinsolyse@users.noreply.github.com>
2026-08-21 19:03:21 +0530

    Initial commit for hairclub-pro-widget

---
+++ app/src/App.jsx
@@ -0,0 +1,11 @@
+import HairClubWidget from './HairClubWidget';
+
+function App() {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '100vh' }}>
+      <HairClubWidget />
+    </div>
+  )
+}
+
+export default App
